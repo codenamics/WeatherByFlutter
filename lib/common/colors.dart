018e9bb7b2e9718c782ca52993a5641714f7faa4
codenamics <info@codenamics.pl>
2019-09-14 21:53:59 +0200

    new color

--- lib/common/colors.dart
+++ lib/common/colors.dart
@@ -2,10 +2,10 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-final Color primaryColor = Color(0xFFFF9F59);
+final Color primaryColor = Color(0xFF4044c9);
 final Color backgroundColor = Color(0xFFE4E6F1);
-final Color buttonColor = Color(0xff0e09fb);
-final Color boxColor = Color(0xff4556FE);
+final Color buttonColor = Color(0xff4044c9);
+final Color boxColor = Color(0xff4044c9);
 final Color firstCircleColor = Colors.white.withOpacity(0.3);
 final Color secondCircleColor = Colors.white.withOpacity(0.4);
 final Color thirdCircleColor = Colors.white.withOpacity(0.6);
