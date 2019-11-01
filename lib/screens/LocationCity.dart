ee53daa52d18c5888eed4a54911b2ab96bc5fecc
Damian Bauer <damianbauer@MacBook-Pro-Damian.local>
2019-11-01 23:04:27 +0100

    softkeyboard dismiss

--- lib/screens/LocationCity.dart
+++ lib/screens/LocationCity.dart
@@ -13,90 +13,88 @@ class LocationCity extends StatefulWidget {
 class _LocationCityState extends State<LocationCity> {
   String text;
   Ripple ripple = Ripple();
-  final Duration animationDuration = Duration(milliseconds: 300);
-  final Duration delay = Duration(milliseconds: 300);
+  final Duration animationDuration = Duration(milliseconds: 500);
+  final Duration delay = Duration(milliseconds: 0);
   GlobalKey rectGetterKey = RectGetter.createGlobalKey();
   Rect rect;
 
   void _onTap() async {
-    
+    FocusScope.of(context).requestFocus(new FocusNode());
     setState(() => rect = RectGetter.getRectFromKey(rectGetterKey));
     WidgetsBinding.instance.addPostFrameCallback((_) {
       setState(() =>
           rect = rect.inflate(1.3 * MediaQuery.of(context).size.longestSide));
       Future.delayed(animationDuration + delay, _goToNextPage);
     });
-    print(rect);
   }
 
   void _goToNextPage() {
     Navigator.of(context)
-        .push(FadeRouteBuilder(page:  Weather(
-                          text: text,
-                        )))
+        .push(FadeRouteBuilder(
+            page: Weather(
+          text: text,
+        )))
         .then((_) => setState(() => rect = null));
   }
 
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
-          Center(
-            child: Container(
-        child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Container(
-                      margin: EdgeInsets.only(bottom: 30),
-                      child: Text(
-                        'WeatherApp',
-                        style: TextStyle(fontSize: 50, fontWeight: FontWeight.w300),
-                      )),
-                  TextField(
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Color(0xffa3a3a3), width: 1),
+        Center(
+          child: Container(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Container(
+                        margin: EdgeInsets.only(bottom: 30),
+                        child: Text(
+                          'WeatherApp',
+                          style: TextStyle(
+                              fontSize: 50, fontWeight: FontWeight.w300),
+                        )),
+                    TextField(
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xffa3a3a3), width: 1),
+                        ),
+                        hintText: 'CityName',
+                        prefixText: ' ',
                       ),
-                      hintText: 'CityName',
-                      prefixText: ' ',
+                      onChanged: (value) {
+                        text = value;
+                      },
                     ),
-                    onChanged: (value) {
-                      text = value;
-                    },
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 10, bottom: 10),
-                  ),
-                  RectGetter(
-                key: rectGetterKey,
-                child: FlatButton(
-                    padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
-                    color: buttonColor,
-                    splashColor: Colors.blueAccent,
-                    onPressed: 
-                      _onTap,
-                   
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5.0)),
-                    child: Text(
-                      'Get Weather',
-                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10, bottom: 10),
                     ),
-                  ),
-              ),
-                  
-                ],
+                    RectGetter(
+                      key: rectGetterKey,
+                      child: FlatButton(
+                        padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
+                        color: buttonColor,
+                        splashColor: Colors.blueAccent,
+                        onPressed: _onTap,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0)),
+                        child: Text(
+                          'Get Weather',
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-        ),
-      ),
           ),
-       ripple.ripple(rect, animationDuration, context),
+        ),
+        ripple.ripple(rect, animationDuration, context),
       ],
-        
     );
   }
 }
