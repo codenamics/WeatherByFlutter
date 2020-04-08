d7415ddea9c3f94ee2ddc4a33996d96dbc1da7b6
Damian <info@codenamics.pl>
2020-04-08 13:02:46 +0200

    upgrade to support Xcode 11.4

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/damianbauer/Desktop/Flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/damianbauer/Desktop/Flutter/WeatherByFlutter"
-export "FLUTTER_TARGET=/Users/damianbauer/Desktop/Flutter/WeatherByFlutter/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/damianbauer/Desktop/Flutter/flutter_weather"
+export "FLUTTER_TARGET=/Users/damianbauer/Desktop/Flutter/flutter_weather/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/damianbauer/Desktop/Flutter/flutter/bin/cache/artifacts/engine/ios"
